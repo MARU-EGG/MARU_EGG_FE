1bc049bdf62b4b7022b0fd24349a64367453d3ad
Choi JunHo <duekzim@gmail.com>
2024-07-02 10:48:13 +0900

    Chore: Setting eslint and etc css

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:storybook/recommended',
+  ],
+  plugins: ['react', '@typescript-eslint', 'prettier', 'storybook'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+  overrides: [
+    {
+      files: ['*.stories.@(ts|tsx|js|jsx|mdx)'],
+      rules: {},
+    },
+  ],
+};
