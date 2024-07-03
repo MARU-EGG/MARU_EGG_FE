1842493bd689deaa6188cbc3f5cbeffdb36b9e93
Choi JunHo <duekzim@gmail.com>
2024-07-03 22:53:03 +0900

    Feat: layout test page

---
+++ src/ui/pages/page-test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AdminLayout from '../components/molecule/admin/Layout/admin-layout';
+
+const PageTest: React.FC = () => {
+  return (
+    <div>
+      <AdminLayout />
+    </div>
+  );
+};
+
+export default PageTest;
