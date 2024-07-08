7a8edc25e0781cef5f7b9f6eefb43298198b0728
Choi JunHo <duekzim@gmail.com>
2024-07-08 10:13:52 +0900

    Feat: tailwind color, screen config setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-blue': '#002968',
+        'primary-selected-bg': 'rgba(0,41,104,0.06)',
+        'text-black': '#000000',
+        'text-white': '#FFFFFF',
+        'border-gray': '#EBEBEB',
+      },
+      screens: {
+        mobile: '360px',
+        desktop: '769px',
+      },
+    },
   },
   plugins: [],
 };
