ca73872bc98630381f375e0fcfd11cb0c41bffc8
Choi JunHo <duekzim@gmail.com>
2024-07-22 16:44:27 +0900

    Feat: Add icon-button atom component storybook

---
+++ src/ui/components/atom/icon/icon-button.stories.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import IconButton from './icon-button';
+import { ReactComponent as MenuIcon } from '../../../../../public/Menu.svg';
+import { ReactComponent as SendIcon } from '../../../../../public/Send.svg';
+import { ReactComponent as XIcon } from '../../../../../public/X.svg';
+import { ReactComponent as RefreshIcon } from '../../../../../public/Refresh.svg';
+
+const meta = {
+  title: 'Example/IconButton',
+  component: IconButton,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+  args: {
+    onClick: () => console.log('Button clicked'),
+  },
+} satisfies Meta<typeof IconButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: <MenuIcon />, // Menu.svg 사용
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    children: <MenuIcon />, // Menu.svg 사용
+    disabled: true,
+  },
+};
+
+// XIcon 사용 스토리
+export const WithXIcon: Story = {
+  args: {
+    children: <XIcon />, // X.svg 사용
+  },
+};
+
+// SendIcon 사용 스토리
+export const WithSendIcon: Story = {
+  args: {
+    children: <SendIcon />, // Send.svg 사용
+  },
+};
+
+// RefreshIcon 사용 스토리
+export const WithRefreshIcon: Story = {
+  args: {
+    children: <RefreshIcon />, // Refresh.svg 사용
+  },
+};
