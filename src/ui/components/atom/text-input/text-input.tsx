e1301277d85baac029e5c6985c80c91fe0137fe2
Choi JunHo <duekzim@gmail.com>
2024-07-24 09:28:40 +0900

    refactor: text-input stories and text-input

---
+++ src/ui/components/atom/text-input/text-input.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { cn } from '../../../../utils/style';
+
+export interface TextInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  value?: string;
+  onValueChange?: (value: string) => void;
+  disabled?: boolean;
+  defaultValue?: string;
+}
+
+const TextInput = React.forwardRef<HTMLInputElement, TextInputProps>(function TextArea(
+  { disabled = false, placeholder, onValueChange, defaultValue, value },
+  ref,
+) {
+  return (
+    <input
+      ref={ref}
+      type="text"
+      className={cn(
+        `w-full rounded-2xl bg-transparent px-5 py-2 text-body3`,
+        `placeholder:text-[#72777A]`,
+        disabled ? 'placeholder:text-red-500' : 'text-black',
+      )}
+      value={value}
+      placeholder={placeholder}
+      disabled={disabled}
+      onChange={(e) => {
+        onValueChange?.(e.target.value);
+      }}
+    />
+  );
+});
+
+export default TextInput;
