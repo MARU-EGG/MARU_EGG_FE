22209285f4f6b3ad7f771044fbdeb5f0b43afb8f
Choi JunHo <duekzim@gmail.com>
2024-07-24 13:28:27 +0900

    Refactor: post-question

--- src/api/post-question.ts
+++ src/api/post-question.ts
@@ -1,24 +1,21 @@
 import { server_axiosInstance } from '../utils/axios';
 
 export async function postQuestion(category: string, type: string, content: string): Promise<any> {
+  const data = {
+    type,
+    category,
+    content,
+  };
   try {
-    const response = await server_axiosInstance.post(
-      '/api/questions',
-      {
-        type: type,
-        category: category,
-        content: content,
+    const response = await server_axiosInstance.post('/api/questions', JSON.stringify(data), {
+      headers: {
+        'Content-Type': 'application/json',
       },
-      {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      },
-    );
+    });
     console.log('upload success', response.data);
     return response.data;
-  } catch (error) {
+  } catch (error: any) {
     console.error('Upload Error', error);
-    throw new Error(`Upload failed: ${(error as any).message}`);
+    throw new Error(`Upload failed: ${error.message}`);
   }
 }
