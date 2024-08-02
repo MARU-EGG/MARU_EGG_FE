755a695d786f4752b26e5ef98ee32efa0e632cb7
Choi JunHo <duekzim@gmail.com>
2024-08-02 15:39:38 +0900

    Refactor: maru-egg page

--- src/ui/pages/maru-egg.tsx
+++ src/ui/pages/maru-egg.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-
 import Header from '../components/molecule/user/header/header';
+import useTypeStore from '../../store/store';
 
 const MaruEgg: React.FC = () => {
+  const { setSelectedType, type } = useTypeStore();
+  const handleButtonClick = (selectedType: 'SUSI' | 'PYEONIP' | 'JEONGSI') => {
+    setSelectedType(selectedType);
+  };
+
   return (
     <div className="min-w-[360px]">
       <div className="bg-background-default"></div>
-      <Header type="수시" />
+      <Header type={type} />
+      <div>
+        <button onClick={() => handleButtonClick('SUSI')}>Select SUSI</button>
+        <button onClick={() => handleButtonClick('PYEONIP')}>Select PYEONIP</button>
+        <button onClick={() => handleButtonClick('JEONGSI')}>Select JEONGSI</button>
+      </div>
     </div>
   );
 };
