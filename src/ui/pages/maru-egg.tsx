9c9a7267ac4e1d744b5f96b8f17c215d8899f7c2
Choi JunHo <duekzim@gmail.com>
2024-08-02 15:10:59 +0900

    Feat: maru-egg main page

---
+++ src/ui/pages/maru-egg.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MaruEgg: React.FC = () => {
+  return (
+    <div className="min-w-[360px]">
+      <div className="bg-background-default"></div>
+    </div>
+  );
+};
+
+export default MaruEgg;
