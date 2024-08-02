921e4b22dbb1aa94571f2816d7f0346a0a367806
Choi JunHo <duekzim@gmail.com>
2024-08-02 17:16:58 +0900

    Rename: store to type-store

--- src/store/type-store.ts
+++ src/store/type-store.ts
@@ -2,12 +2,12 @@ import { create } from 'zustand';
 
 interface TypeState {
   type: null | 'SUSI' | 'PYEONIP' | 'JEONGSI';
-  setSelectedType: (button: TypeState['type']) => void; // button 타입을 TypeState['type']로 정의
+  setSelectedType: (button: TypeState['type']) => void;
 }
 
 const useTypeStore = create<TypeState>()((set) => ({
   type: null,
-  setSelectedType: (button) => set({ type: button }), // setSelectedType 정의
+  setSelectedType: (button) => set({ type: button }),
 }));
 
 export default useTypeStore;
