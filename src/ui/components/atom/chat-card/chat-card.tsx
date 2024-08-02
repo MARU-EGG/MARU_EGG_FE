91cc6a8923d8c5528396cd69e396de56138ec07a
Choi JunHo <duekzim@gmail.com>
2024-08-02 15:38:55 +0900

    Refactor: add maru egg image

--- src/ui/components/atom/chat-card/chat-card.tsx
+++ src/ui/components/atom/chat-card/chat-card.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import { cn } from '../../../../utils/style';
-
+import maru from '../../../../assets/maru-egg.png';
 interface ChatCardProps {
   content: string;
   role: 'user' | 'system';
@@ -10,20 +10,23 @@ interface ChatCardProps {
 
 const ChatCard = ({ content, role }: ChatCardProps) => {
   return (
-    <div
-      className={cn('flex h-auto flex-col-reverse', {
-        'items-end justify-end': role === 'user',
-        'items-start justify-start': role !== 'user',
-      })}
-    >
+    <div>
+      {role === 'user' ? null : <img src={maru} className="mb-2 h-8 w-8" />}
       <div
-        className={cn('flex w-auto max-w-72 rounded-md bg-white px-5 py-3 text-black', {
-          'justify-end bg-primary-blue text-white': role === 'user',
-          'justify-start text-left': role !== 'user',
+        className={cn('flex h-auto flex-col-reverse', {
+          'items-end justify-end': role === 'user',
+          'items-start justify-start': role !== 'user',
         })}
       >
-        <div className="px-4 py-3 text-body1">
-          <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+        <div
+          className={cn('flex w-auto max-w-72 rounded-md bg-white px-5 py-3 text-black', {
+            'justify-end bg-primary-blue text-white': role === 'user',
+            'justify-start text-left': role !== 'user',
+          })}
+        >
+          <div className="px-4 py-3 text-body1">
+            <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+          </div>
         </div>
       </div>
     </div>
