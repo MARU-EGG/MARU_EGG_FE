e6cc7e871199a033093a40a2ecc4c729926a24fd
Choi JunHo <duekzim@gmail.com>
2024-08-05 10:57:38 +0900

    refactor: routes refactor and feat questionCheck admin routes

--- src/routes/admin-routes.tsx
+++ src/routes/admin-routes.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import PrivateRoute from './private-route';
 import FileList from '../ui/pages/admin/file-list';
-import AdminLayout from '../ui/components/admin/Layout/admin-layout';
 import Login from '../ui/pages/admin/login';
+import QuestionCheck from '../ui/pages/admin/question-check';
+import AdminLayout from '../ui/components/admin/Layout/admin-layout';
 
 const AdminRoutes: React.FC = () => {
   const token = sessionStorage.getItem('Authorization');
@@ -12,6 +13,7 @@ const AdminRoutes: React.FC = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/" element={<PrivateRoute component={<AdminLayout />} authenticated={token} />}>
         <Route path="setting/file" element={<FileList />} />
+        <Route path="question/list" element={<QuestionCheck />} />
       </Route>
     </Routes>
   );
--- src/routes/app-routes.tsx
+++ src/routes/app-routes.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import AdminRoutes from './admin-routes';
-import PageTest from '../ui/pages/page-test';
 import MaruEgg from '../ui/pages/maru-egg';
 
 const AppRoutes: React.FC = () => {
@@ -9,7 +8,6 @@ const AppRoutes: React.FC = () => {
     <Routes>
       <Route path="/" element={<MaruEgg />} />
       <Route path="/admin/*" element={<AdminRoutes />} />
-      <Route path="/test/*" element={<PageTest />} />
     </Routes>
   );
 };
