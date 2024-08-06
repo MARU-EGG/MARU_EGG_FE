cb153ec1379ff53b3ef77b192a9f41d23f39ac92
Choi JunHo <duekzim@gmail.com>
2024-08-06 13:57:47 +0900

    Refactor: Add logout button

--- src/ui/components/admin/Layout/admin-layout.tsx
+++ src/ui/components/admin/Layout/admin-layout.tsx
@@ -1,18 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import { Breadcrumb, Layout, Menu } from 'antd';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Breadcrumb, Button, Layout, Menu } from 'antd';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import SlideMenu from '../slide-menu/slide-menu';
 import { Header } from 'antd/es/layout/layout';
+import { AdminLogout } from '../../../../api/admin-login';
+import { removeCookie } from '../../../../utils/cookies';
 
 const { Sider, Content, Footer } = Layout;
 
 const AdminLayout: React.FC = () => {
   const { pathname } = useLocation();
+  const navigate = useNavigate();
   useEffect(() => {
     pathname.split('/').map((a, i) => {
       console.log(pathname.split('/')[i]);
     });
   });
+
+  const handleLogout = () => {
+    AdminLogout();
+    navigate('/admin/login');
+  };
 
   const breadcrumpItem1 = pathname.split('/')[1];
   const breadcrumpItem2 = pathname.split('/')[2];
@@ -24,8 +32,11 @@ const AdminLayout: React.FC = () => {
         <SlideMenu />
       </Sider>
       <Layout className="site-layout">
-        <Header className="flex items-center bg-[#DFF4FF]">
+        <Header className="flex items-center justify-between bg-white">
           <Breadcrumb separator=">" items={breadcrumpItems} className="pl-2 text-lg text-black" />
+          <Button type="primary" onClick={handleLogout}>
+            로그아웃
+          </Button>
         </Header>
         <Content style={{ margin: '16px 0' }}>
           <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
