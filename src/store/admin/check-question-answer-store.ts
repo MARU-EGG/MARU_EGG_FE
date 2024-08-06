acc1d902a6f65873021e4daba8a73ccb1fe1a3ac
Choi JunHo <duekzim@gmail.com>
2024-08-07 02:15:19 +0900

    Refactor: add store code annotation

--- src/store/admin/check-question-answer-store.ts
+++ src/store/admin/check-question-answer-store.ts
@@ -13,10 +13,10 @@ export interface QuestionAnswerState {
 
 interface CheckQuestionAnswerState {
   questionData: QuestionAnswerState[];
-  updateQuestionData: (firstData: QuestionAnswerState[]) => void;
-  inputQuestionData: (data: QuestionAnswerState) => void;
-  updateCheck: (id: number, isChecked: boolean) => void;
-  updateAnswer: (answerId: number, answerContent: string) => void;
+  updateQuestionData: (firstData: QuestionAnswerState[]) => void; //처음 데이터를 삽입할 때 사용하는 거
+  inputQuestionData: (data: QuestionAnswerState) => void; //커스텀 질문 생성시 사용하는거
+  updateCheck: (id: number, isChecked: boolean) => void; //질문-답변 상태 확인할 때 사용
+  updateAnswer: (answerId: number, answerContent: string) => void; //답변 내용 변경할 때 사용
 }
 
 const useCheckQuestionAnswerStore = create<CheckQuestionAnswerState>((set) => ({
