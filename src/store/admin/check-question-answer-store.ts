42d042d92cfc82490b2b9fdaed0ed2935150f184
Choi JunHo <duekzim@gmail.com>
2024-08-07 02:09:44 +0900

    Feat: check question answer store

---
+++ src/store/admin/check-question-answer-store.ts
@@ -0,0 +1,40 @@
+import { create } from 'zustand';
+
+export interface QuestionAnswerState {
+  id: number;
+  content: string;
+  viewCount: string;
+  isChecked: boolean;
+  answer: {
+    id: number;
+    content: string;
+  };
+}
+
+interface CheckQuestionAnswerState {
+  questionData: QuestionAnswerState[];
+  updateQuestionData: (firstData: QuestionAnswerState[]) => void;
+  inputQuestionData: (data: QuestionAnswerState) => void;
+  updateCheck: (id: number, isChecked: boolean) => void;
+  updateAnswer: (answerId: number, answerContent: string) => void;
+}
+
+const useCheckQuestionAnswerStore = create<CheckQuestionAnswerState>((set) => ({
+  questionData: [],
+  updateQuestionData: (firstData) => set({ questionData: firstData }),
+  inputQuestionData: (data) => set((state) => ({ questionData: [...state.questionData, data] })),
+  updateCheck: (id, isChecked) =>
+    set((state) => ({
+      questionData: state.questionData.map((question) => (question.id === id ? { ...question, isChecked } : question)),
+    })),
+  updateAnswer: (answerId, answerContent) =>
+    set((state) => ({
+      questionData: state.questionData.map((question) =>
+        question.answer.id === answerId
+          ? { ...question, answer: { ...question.answer, content: answerContent } }
+          : question,
+      ),
+    })),
+}));
+
+export default useCheckQuestionAnswerStore;
