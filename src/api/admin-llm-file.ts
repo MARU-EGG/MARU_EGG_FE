6b94fa066b235cd1e3e073476a452451e2a6e7f2
Choi JunHo <duekzim@gmail.com>
2024-08-14 10:46:39 +0900

    Feat: admin-delete-file api

---
+++ src/api/admin-llm-file.ts
@@ -0,0 +1,34 @@
+import { llm_axiosInstance } from '../utils/axios';
+
+interface AdminLLMFileProps {
+  type?: string;
+  category?: string;
+}
+
+export async function adminRetrieveFile({ type, category }: AdminLLMFileProps) {
+  try {
+    const response = await llm_axiosInstance.get('/retrieve_documents', {
+      params: {
+        type,
+        category,
+      },
+    });
+    return response.data;
+  } catch (error: any) {
+    throw new Error(`Upload failed: ${error.message}`);
+  }
+}
+
+export async function adminDeleteFile({ type, category }: AdminLLMFileProps) {
+  try {
+    const response = await llm_axiosInstance.delete('/delete_documents', {
+      params: {
+        type,
+        category,
+      },
+    });
+    return response.data;
+  } catch (error: any) {
+    throw new Error('Delete failed');
+  }
+}
