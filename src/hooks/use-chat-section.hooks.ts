6f0ebc0d10603b9ef735141367ae28804e7c4205
Choi JunHo <duekzim@gmail.com>
2024-08-22 22:51:57 +0900

    Feat: add use-chat-section hook

---
+++ src/hooks/use-chat-section.hooks.ts
@@ -0,0 +1,27 @@
+import React from 'react';
+import useTypeStore, { TypeCategoryState } from '../store/type-category-store';
+
+const useChatSection = () => {
+  const { setSelectedType, setSelectedCategory } = useTypeStore();
+  const [selectedTypeButton, setSelectedTypeButton] = React.useState<TypeCategoryState['type']>(undefined);
+  const [selectedCategoryButton, setSelectedCategoryButton] = React.useState<TypeCategoryState['category']>(undefined);
+
+  const handleTypeButtonClick = (selectedType: TypeCategoryState['type']) => {
+    setSelectedType(selectedType);
+    setSelectedTypeButton(selectedType);
+  };
+
+  const handleCategoryButtonClick = (selectedCategory: TypeCategoryState['category']) => {
+    setSelectedCategory(selectedCategory);
+    setSelectedCategoryButton(selectedCategory);
+  };
+
+  return {
+    selectedTypeButton,
+    selectedCategoryButton,
+    handleTypeButtonClick,
+    handleCategoryButtonClick,
+  };
+};
+
+export default useChatSection;
