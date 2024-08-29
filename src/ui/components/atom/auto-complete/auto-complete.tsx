5e2c192f758781bf67cb51697a7ea9c36d6ac23c
Choi JunHo <duekzim@gmail.com>
2024-08-29 10:14:17 +0900

    refactor: add isChecked autocomplete logic

--- src/ui/components/atom/auto-complete/auto-complete.tsx
+++ src/ui/components/atom/auto-complete/auto-complete.tsx
@@ -14,15 +14,18 @@ const AutoCompleteList: React.FC<AutoCompleteListProps> = ({ results, onSelect }
   return (
     <div className="mb-4">
       <ul className="z-10 rounded-md bg-white">
-        {results.map((result, index) => (
-          <li
-            key={index}
-            className="cursor-pointer px-[16px] py-2 font-pretendard font-normal text-[#3A3A3A] hover:rounded-md hover:bg-[#F7F7F7]"
-            onClick={() => onSelect(result.content, result.id)}
-          >
-            {result.content}
-          </li>
-        ))}
+        {results.map(
+          (result, index) =>
+            result.isChecked && (
+              <li
+                key={index}
+                className="cursor-pointer px-[16px] py-2 font-pretendard font-normal text-[#3A3A3A] hover:rounded-md hover:bg-[#F7F7F7]"
+                onClick={() => onSelect(result.content, result.id)}
+              >
+                {result.content}
+              </li>
+            ),
+        )}
       </ul>
     </div>
   );
