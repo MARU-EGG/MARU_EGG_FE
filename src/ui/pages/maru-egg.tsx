4a52a8ebfb396039a553a8a55e21558934985b10
Choi JunHo <duekzim@gmail.com>
2024-09-02 22:32:25 +0900

    refactor: remove other import

--- src/ui/pages/maru-egg.tsx
+++ src/ui/pages/maru-egg.tsx
@@ -2,10 +2,7 @@
 import React from 'react';
 import Header from '../components/molecule/header/header';
 import useTypeStore from '../../store/type-category-store';
-import ChatCard from '../components/atom/chat-card/chat-card';
 import ChatForm from '../components/molecule/chat-form/chat-form';
-import useChatStore from '../../store/chat-store';
-import PresetButton from '../components/atom/preset/preset-button';
 import ChatSection from '../components/molecule/chat-section/chat-section';
 
 const MaruEgg: React.FC = () => {
@@ -15,6 +12,7 @@ const MaruEgg: React.FC = () => {
     <div className="flex h-svh items-center justify-center bg-gray-100">
       <div className="relative flex h-full w-full bg-background-default mobile:h-full mobile:min-h-[480px] mobile:min-w-[320px] mobile:rounded-none desktop:h-[780px] desktop:max-w-[390px] desktop:rounded-3xl desktop:border desktop:border-gray-200 desktop:shadow-2xl">
         <Header type={type} />
+
         <ChatSection />
         {type !== undefined && category !== undefined && (
           <div className="absolute bottom-0 w-full bg-white px-3 py-3 mobile:rounded-none desktop:rounded-bl-3xl desktop:rounded-br-3xl">
