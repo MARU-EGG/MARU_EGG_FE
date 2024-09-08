022c4c6ebecb818d46711ec7b978967e5ac8f31c
sangmaaaaan <matthew10164@gmail.com>
2024-09-08 13:54:35 +0900

    fix: 불필요한 screen size 제거

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,6 @@ module.exports = {
         'background-default': '#F2F2F3',
       },
       screens: {
-        'mobile_sm' : '320px',
         'mobile' : '360px',
         'desktop' : '769px',
       },
