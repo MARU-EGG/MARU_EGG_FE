54106ba98bad6c7f8829d563e8a728c2de917ba1
sangmaaaaan <matthew10164@gmail.com>
2024-09-08 20:42:52 +0900

    Feat: onboarding modal page add

--- src/ui/pages/maru-egg.tsx
+++ src/ui/pages/maru-egg.tsx
@@ -1,18 +1,24 @@
 // src/ui/pages/maru-egg.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/molecule/header/header';
 import useTypeStore from '../../store/type-category-store';
 import ChatForm from '../components/molecule/chat-form/chat-form';
 import ChatSection from '../components/molecule/chat-section/chat-section';
+import Onboarding from '../components/molecule/onboarding/onboarding';
 
 const MaruEgg: React.FC = () => {
   const { type, category } = useTypeStore();
+  const [showOnboarding, setShowOnboarding] = useState(true);
+
+  const handleCloseOnboarding = () => {
+    setShowOnboarding(false);
+  };
 
   return (
     <div className="flex h-svh items-center justify-center bg-gray-100">
       <div className="relative flex h-full w-full bg-background-default mobile:h-full mobile:min-h-[480px] mobile:min-w-[320px] mobile:rounded-none desktop:h-[780px] desktop:max-w-[390px] desktop:rounded-3xl desktop:border desktop:border-gray-200 desktop:shadow-2xl">
+        {showOnboarding && <Onboarding onClose={handleCloseOnboarding} />}
         <Header type={type} />
-
         <ChatSection />
         {type !== undefined && category !== undefined && (
           <div className="absolute bottom-0 w-full bg-white px-3 py-3 mobile:rounded-none desktop:rounded-bl-3xl desktop:rounded-br-3xl">
