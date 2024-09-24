8640e435c9b1fefc35b666bea92ed6b986b00174
Choi JunHo <duekzim@gmail.com>
2024-09-24 17:31:45 +0900

    refactor: remove referencebuttonclick hook in chat-section

--- src/hooks/use-chat-section.hooks.ts
+++ src/hooks/use-chat-section.hooks.ts
@@ -1,13 +1,10 @@
 import React from 'react';
 import useTypeStore, { TypeCategoryState } from '../store/type-category-store';
-import useChatStore, { referenceState } from '../store/chat-store';
 
 const useChatSection = () => {
   const { setSelectedType, setSelectedCategory } = useTypeStore();
-  const { addMessage, updateReferenceDisabled } = useChatStore();
   const [selectedTypeButton, setSelectedTypeButton] = React.useState<TypeCategoryState['type']>(undefined);
   const [selectedCategoryButton, setSelectedCategoryButton] = React.useState<TypeCategoryState['category']>(undefined);
-
   const handleTypeButtonClick = (selectedType: TypeCategoryState['type']) => {
     setSelectedType(selectedType);
     setSelectedTypeButton(selectedType);
@@ -18,27 +15,11 @@ const useChatSection = () => {
     setSelectedCategoryButton(selectedCategory);
   };
 
-  const handleReferenceButtonClick = (references: referenceState[]) => {
-    console.log(references);
-    let content = '답변 출처를 알려드릴게요!\n';
-    if (references === undefined || references.length === 0) {
-      addMessage({ content: '출처 정보가 없습니다.', role: 'system' });
-      return;
-    }
-    references.map((reference, index) => {
-      content += `\n출처${index + 1}번: ${reference.link}\n`;
-    });
-    content += '\n**답변을 꼭 확인해주세요!**';
-    addMessage({ content: content, role: 'system' });
-    updateReferenceDisabled(true);
-  };
-
   return {
     selectedTypeButton,
     selectedCategoryButton,
     handleTypeButtonClick,
     handleCategoryButtonClick,
-    handleReferenceButtonClick,
   };
 };
 
