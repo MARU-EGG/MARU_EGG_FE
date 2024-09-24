99d591ea5521682be3781d25b65f46c6d32320e8
Choi JunHo <duekzim@gmail.com>
2024-09-24 17:32:26 +0900

    feat: use-preset-button hoooks and presetbuttons

---
+++ src/hooks/use-preset-button.hooks.ts
@@ -0,0 +1,55 @@
+import useChatStore, { referenceState } from '../store/chat-store';
+import useTypeStore from '../store/type-category-store';
+import { postQuestion } from '../api/post-question';
+import { SearchById } from '../api/admin/question-manage/admin-search-by-id';
+
+const usePresetButton = () => {
+  const { type, category } = useTypeStore();
+  const { addMessage, setLoading, updateLastMessage, updateLastReference, updateReferenceDisabled } = useChatStore();
+
+  const handleReferenceButtonClick = (references: referenceState[]) => {
+    let content = '답변 출처를 알려드릴게요!\n';
+    if (references === undefined || references.length === 0) {
+      addMessage({ content: '출처 정보가 없습니다.', role: 'system' });
+      return;
+    }
+    references.map((reference, index) => {
+      content += `\n출처${index + 1}번: ${reference.link}\n`;
+    });
+    content += '\n**답변을 꼭 확인해주세요!**';
+    addMessage({ content: content, role: 'system' });
+    updateReferenceDisabled(true);
+  };
+
+  const fetchResponse = async (question: string) => {
+    return await postQuestion(category, type, question);
+  };
+
+  const updateStateWithResponse = (response: any) => {
+    updateLastMessage(response.answer.content);
+    updateLastReference(response.references);
+    updateReferenceDisabled(false);
+    setLoading(false);
+  };
+
+  const handleButtonClick = async (question: string) => {
+    try {
+      addMessage({ content: question, role: 'user' });
+      addMessage({ content: 'loading', role: 'system' });
+      setLoading(true);
+
+      const response = await fetchResponse(question);
+      updateStateWithResponse(response);
+    } catch (error) {
+      setLoading(false);
+      updateLastMessage('답변 생성에 실패했습니다. 새로고침해주세요');
+    }
+  };
+
+  return {
+    handleReferenceButtonClick,
+    handleButtonClick,
+  };
+};
+
+export default usePresetButton;
