15a339d201d8059067ece2bc28e6b78effdb679e
sangmaaaaan <matthew10164@gmail.com>
2024-10-02 05:33:38 +0900

    style: enhance button design more clickable

--- src/ui/components/atom/preset/preset-button.tsx
+++ src/ui/components/atom/preset/preset-button.tsx
@@ -10,9 +10,13 @@ interface PresetButtonProps {
 
 const PresetButton = ({ onClick, isSelected, disabled, children }: PresetButtonProps) => {
   const buttonClasses = cn(
-    'cursor-pointer rounded-full border border-gray text-black bg-white px-4 py-2 ',
-    isSelected ? 'bg-primary-blue text-white border-primary-blue' : 'bg-white text-black',
-    disabled ? 'bg-border-gray cursor-not-allowed text-border-gray bg-text-white' : '',
+    'cursor-pointer text-base rounded-full border border-gray text-black bg-white px-4 py-2 transition-colors duration-200',
+    isSelected
+      ? 'bg-primary-blue text-white border-gray'
+      : 'bg-white text-black hover:bg-primary-blue hover:text-white',
+    disabled
+      ? 'bg-border-gray cursor-not-allowed text-border-gray bg-text-white hover:text-border-gray hover:bg-text-white'
+      : '',
   );
 
   return (
