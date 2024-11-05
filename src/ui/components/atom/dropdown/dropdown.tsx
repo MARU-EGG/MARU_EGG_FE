148365ff9198094200da300f5e4409660b75bac5
Choi JunHo <duekzim@gmail.com>
2024-11-05 23:51:25 +0900

    refactor: add dropdown event

--- src/ui/components/atom/dropdown/dropdown.tsx
+++ src/ui/components/atom/dropdown/dropdown.tsx
@@ -1,24 +1,40 @@
 import React from 'react';
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 import { ChevronRightIcon } from '@radix-ui/react-icons';
+import { useDetailTypePrompt } from '../../../../hooks/use-detail-type-prompt.hooks';
+import { useUserDetailTypeStore } from '../../../../store/user-detail-type-store';
+import usePresetButton from '../../../../hooks/use-preset-button.hooks';
 
 interface DropdownProps {
   type: 'SUSI' | 'JEONGSI' | 'PYEONIP';
   items: { middleName: string; lastNames: string[] }[];
 }
 
-const Dropdown: React.FC<DropdownProps> = ({ type, items }) => {
+const Dropdown: React.FC<DropdownProps> = ({ type }) => {
+  const { itemList: items } = useDetailTypePrompt();
+  const { setSelectedName } = useUserDetailTypeStore();
+  const { handleDetailTypeButtonClick } = usePresetButton();
+
+  const handleNameClick = (name: string) => {
+    setSelectedName(name);
+    handleDetailTypeButtonClick(name);
+  };
+
   return (
     <DropdownMenu.Root open>
       <DropdownMenu.Trigger asChild>
-        <button style={{ display: 'none' }} />
+        <button style={{ display: 'hidden' }}></button>
       </DropdownMenu.Trigger>
       <DropdownMenu.Portal>
-        <DropdownMenu.Content className="rounded border border-gray-300 bg-white shadow-lg">
+        <DropdownMenu.Content className="rounded-md border-gray-300 bg-white" align="start">
           {type === 'PYEONIP'
             ? // 편입의 경우, 한 번에 전체 목록 표시
               items.map((item, index) => (
-                <DropdownMenu.Item key={index} className="cursor-pointer px-4 py-2 hover:bg-gray-100">
+                <DropdownMenu.Item
+                  key={index}
+                  className="cursor-pointer px-4 py-2 hover:bg-gray-100"
+                  onClick={() => handleNameClick(item.middleName)}
+                >
                   {item.middleName}
                 </DropdownMenu.Item>
               ))
@@ -30,9 +46,13 @@ const Dropdown: React.FC<DropdownProps> = ({ type, items }) => {
                     <ChevronRightIcon />
                   </DropdownMenu.SubTrigger>
                   <DropdownMenu.Portal>
-                    <DropdownMenu.SubContent className="rounded border border-gray-300 bg-white shadow-lg">
+                    <DropdownMenu.SubContent className="rounded-md border border-gray-300 bg-white shadow-lg">
                       {item.lastNames.map((lastName, subIndex) => (
-                        <DropdownMenu.Item key={subIndex} className="cursor-pointer px-4 py-2 hover:bg-gray-100">
+                        <DropdownMenu.Item
+                          key={subIndex}
+                          className="cursor-pointer px-4 py-2 hover:bg-gray-100"
+                          onClick={() => handleNameClick(lastName)}
+                        >
                           {lastName}
                         </DropdownMenu.Item>
                       ))}
