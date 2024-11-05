694d59a229e64662419e85bf301b4dd2275be36c
Choi JunHo <duekzim@gmail.com>
2024-11-06 01:03:32 +0900

    refactor: axios timeout setting plus

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,12 +2,12 @@ import axios from 'axios';
 
 export const llm_axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_LLM_SERVER_API_ADDRESS,
-  timeout: 20000,
+  timeout: 200000,
   withCredentials: false,
 });
 
 export const server_axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_SPRING_SERVER_API_ADDRESS,
-  timeout: 20000,
+  timeout: 200000,
   withCredentials: true,
 });
